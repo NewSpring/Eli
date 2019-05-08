e7e07c54e2ebdfbcc8cc7466898ae37cc688a8b3
Michael Neeley <micneeley14@gmail.com>
2019-05-08 11:45:45 -0400

    removed stories

--- src/.storybook/web/config.js
+++
@@ -1,7 +0,0 @@
-import { configure, addDecorator } from '@storybook/react';
-import { loadStories } from '../storyLoader'; // storyLoader.js is generated on storybook start
-import AppContext from '../decorators/AppContext';
-
-addDecorator(AppContext);
-
-configure(loadStories, module);
--- src/@ui/TabBar/TabBar.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react-native';
-
-import TabBar from '.';
-
-storiesOf('TabBar', module)
-  .add('renders', () => <TabBar />);
